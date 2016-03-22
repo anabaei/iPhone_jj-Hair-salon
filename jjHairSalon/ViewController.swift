9d1991fb0a3c361d8d2c1d77d7fced04ef93b429
Amir Na <amirn3@icloud.com>
2016-03-22 13:39:13 -0700

    Initial Commit

---
+++ jjHairSalon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jjHairSalon
+//
+//  Created by Amir Nabaei on 2016-03-22.
+//  Copyright © 2016 Amir Nabaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
